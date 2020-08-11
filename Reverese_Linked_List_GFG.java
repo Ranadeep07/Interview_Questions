a79b3fd2c04ca97f190c2a75166c1992fda73160
Ranadeep07 <60144892+Ranadeep07@users.noreply.github.com>
2020-08-11 08:31:17 +0530

    Create Reverese_Linked_List_GFG.java

---
+++ Reverese_Linked_List_GFG.java
@@ -0,0 +1,86 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class GFG{
+	static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+    public static void main(String args[]) throws IOException { 
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t > 0){
+        
+        	int n = sc.nextInt();
+        
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+        
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+        
+            ReverseLL g = new ReverseLL();
+            head = g.reverseList(head);
+            printList(head); 
+            t--;
+        }
+    } 
+} 
+   // } Driver Code Ends
+
+
+//function Template for Java
+
+/* linked list node class:
+
+class Node {
+    int value;
+    Node next;
+    Node(int value) {
+        this.value = value;
+    }
+}
+
+*/
+
+class ReverseLL
+{
+    // This function should reverse linked list and return
+    // head of the modified linked list.
+    Node reverseList(Node head)
+    {
+        if(head == null) return head;
+        Node temp = head;
+        Node prev = null;
+        while(temp!=null)
+        {
+            Node next = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = next;
+            
+        }
+        return prev;
+    }
+}
